530b2e8c7fd12cdbdd274db42e5f7bdecd13c8db
HanUL072 <dbstjrals125@naver.com>
2025-06-10 14:01:17 +0900

    [학습] JavaScript 변수(변할 수 있는) - 시간의 흐름에 따라 값이 변할 수 있음 - 변수 선언 - 선언된 변수에 값 할당

--- learn/05-variables.js
+++ learn/05-variables.js
@@ -33,10 +33,23 @@ console.log('initialApples = ', $initialApples )
 // let dark-impact = '다크 임팩트' [x]
 
 // 추가로 구매한 사과의 갯수 = 27
-// 변수 선언 및 값 할당
 //let 추가로_구매한_사과의_갯수 = 27
+
+
+// 변수 선언 및 값 할당
+let addBuyApples //기본값 (undefined)
+// let addBuyApples = undefined (암묵적)
+console.log('addBuyApples =', addBuyApples) //undefined
+
+//선언된 변수에 값 할당
+addBuyApples = 27
+console.log('addBuyApples =', addBuyApples) //undefined → 27
+
 //console.log('추가로_구매한_사과의_갯수 = ', 추가로_구매한_사과의_갯수) //27
 
 // 현재 내가 가진 사과의 갯수 = 31
 //let 현재_내가_가진_사과의_갯수 = 처음_가진_사과의_갯수 + 추가로_구매한_사과의_갯수
-//console.log('현재_내가_가진_사과의_갯수 = ', 현재_내가_가진_사과의_갯수) //31
+//console.log('현재_내가_가진_사과의_갯수 = ', 현재_내가_가진_사과의_갯수) // 31
+
+// let 변수 vs. var 변수
+// let 변수 vs. const 변수(상수)
