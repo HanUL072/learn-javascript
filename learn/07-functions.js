8f4dd3a71f5a431e9f9f74df00bff330bb1d1d2e
HanUL072 <dbstjrals125@naver.com>
2025-06-10 16:25:12 +0900

    [실습] JavaScript 함수 매개변수 - 함수이름 옆 소괄호() 안에 선언된 변수 - 이런 변수를 특별히 매개변수라고 부름 - 함수 안에서만 접근 가능(지역 변수 같은 느낌) - 함수 호출(실행)할 때 전달한 값이 매개변수 값으로 할당

--- learn/07-functions.js
+++ learn/07-functions.js
@@ -14,6 +14,8 @@ console.log('함수(Function)')
 // drawWater 함수 부르기(call)
 // drawWater 함수 부르기(call)
 
+
+// ------------------------------------------------------------------------
 // 함수 선언 (Function Declaration) 구문
 function drawWater() {
   console.groupCollapsed('drawWater() 호출됨')
@@ -25,6 +27,7 @@ function drawWater() {
 }
 
 
+// ------------------------------------------------------------------------
 // 함수 사용 (Call Function)
 // 재사용성 (Reusability)
 drawWater() 
@@ -46,3 +49,39 @@ drawWater()
 // function cont inue() {}
 // function cla--ss() {}
 // function for() {}
+
+// ------------------------------------------------------------------
+
+// 함수 매개변수 (Function Parameters)
+
+// 함수 밖에 있는 [ 전역 ]변수 (global scope ariables)
+// 코드 실행 범위 어디서나 접근이 가능한 변수
+let year = 4027
+
+// 함수 선언
+function writeDiary(연도, 월, 일){
+    // 함수이름 옆 () 안에 선언된 변수를 특별히 매개변수라 부른다.
+    console.log(연도, 월, 일)
+    // 함수 내부에 선언된 변수
+    // 함수 안에서 사용
+    // 지역 변수 (local scope variables)
+
+    // 연도(year)
+    let year = 2025
+    // 월(month)
+    let month = 6
+    // 일(date)
+    let date = 10
+
+    //year = 2026
+
+    // 변수 + 문자 = 문장
+    // 변수('문자') + 변수(문자) = 문자 연결
+    console.log(연도 + '년 ' +  월 + '월 ' + 일 +'일에 일기를 쓴다.')
+}
+
+// 함수 호출(실행)
+writeDiary(2025, 6, 10)
+writeDiary(2025, 7, 1)
+// writeDiary(연도, 월, 일)
+// writeDiary(연도, 월, 일)
