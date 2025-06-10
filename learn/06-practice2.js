9180469735bd85de96de8f3bb1a0d770b1a0e7c6
HanUL072 <dbstjrals125@naver.com>
2025-06-10 14:59:37 +0900

    [실습] 변수 및 상수 이름 작성 (의미 부여) - 변하지 않는 것 - 변하는 것

---
+++ learn/06-practice2.js
@@ -0,0 +1,83 @@
+//1. `const` 키워드를 사용해 값이 변하지 않는 상수를 선언해봅니다.
+    //1. "하루는 24시간"
+    const hours_in_day = 24
+    console.log(hours_in_day)
+
+    //2. "물의 어는 점은 섭씨 0도"
+    const frezzing_point_celsius = 0
+    console.log(frezzing_point_celsius)
+
+    //3. "삼각형 내각의 합은 180도"
+    const triangle_angle_sum = 180
+    console.log(triangle_angle_sum)
+
+    //4. "지구의 중력 가속도는 9.8 m/s²"
+    const earth_gravity = '9.8 m/s²'
+    console.log(earth_gravity)
+
+// 2. 다음 내용을 코드로 작성해 결과 값을 콘솔 패널에 출력해보세요.Add commentMore actions
+//     1. `let` 키워드를 사용해 `sum` 변수를 선언하고 값을 `0`으로 설정합니다.
+let sum = 0
+console.log('sum =', sum)
+//     2. `num1`, `num2` 변수를 선언하고, 각각 `300`, `50`으로 값을 설정합니다.
+
+// [1]
+let num1 = 300
+let num2 = 50
+
+// [2]
+// let num1, num2
+// num1 = 300
+// num2 = 50
+
+// [3]
+// let num1 = 300, num2 = 50
+
+// [4]
+// let num1 = 300, 
+//     num2 = 50,
+//     num3 = -90
+
+//     3. `num1`, `num2`의 합을 `sum` 변수에 할당합니다.
+sum = num1 + num2
+console.log('sum =', sum)
+
+
+//3. 사람 이름, 나이, 오늘의 기온, 장바구니 상품 갯수, 집 주소, 현재 시간,
+//로그인 상태, 피자 가격, 학생 수 등에 걸맞는 변수 이름을 정하고 적절한 값을 설정합니다.
+
+// 사람 이름
+let personName = 'mina'
+console.log('personName =',personName)
+
+// 나이
+let age = 18
+console.log('age=', age)
+
+// 오늘의 기온
+let todayTemperature = '25도'
+console.log('todayTemperature=', todayTemperature)
+
+// 장바구니 상품 갯수
+let cartItemCount = 12
+console.log('todayTemperature=', todayTemperature)
+
+// 집 주소
+let homeAdress = '경기도 안성시'
+console.log('homeAdress=', homeAdress)
+
+// 현재 시간
+let currentTime = '2시 37분'
+console.log('currentTime=', currentTime)
+
+// 로그인 상태
+let isLoggedIn = false
+console.log('isLoggedIn=', isLoggedIn)
+
+// 피자가격
+let pizzaPrice = 22_000
+console.log('pizzaPrice=', pizzaPrice)
+
+//학생 수
+let studentCount = 43
+console.log('studentCount=', studentCount)
