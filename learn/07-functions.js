a2c45ada9e0dd574765e18b9db93735cafb43a7c
HanUL072 <dbstjrals125@naver.com>
2025-06-10 15:47:54 +0900

    [실습] JavaScript 함수 - 함수는 "기능" - 함수는 동사(get~, is~, have~) - 함수 선언 - function 키워드 - 함수 호출(실행) - 소괄호 ()를 붙이면 실행(호출) - 함수 이름 작성 규칙은 변수와 유사 - 직관적이고 명확하게 함수 이름 짓기 생활화

---
+++ learn/07-functions.js
@@ -0,0 +1,48 @@
+console.log('함수(Function)')
+
+// 함수란?
+// 특정 순서에 따라 작업을 실행하는 코드묶음(블럭)
+
+// drawWater 함수 [
+//   1. 빈 양동이 가져오기
+//   2. 양동이 들고 우물로 이동
+//   3. 우물에서 물 길어오기
+//   4. 집으로 귀가하기
+// ]
+
+// drawWater 함수 부르기(call)
+// drawWater 함수 부르기(call)
+// drawWater 함수 부르기(call)
+
+// 함수 선언 (Function Declaration) 구문
+function drawWater() {
+  console.groupCollapsed('drawWater() 호출됨')
+      console.log('1. 빈 양동이 가져오기')
+      console.log('2. 양동이 들고 우물로 이동')
+      console.log('3 우물에서 물 길어오기')
+      console.log('4. 집으로 귀가하기')
+  console.groupEnd()
+}
+
+
+// 함수 사용 (Call Function)
+// 재사용성 (Reusability)
+drawWater() 
+
+// Q. 함수이름() <- 이거 왜 붙이는 건가요?
+// 동사형태
+// 함수 "호출(실행)한다" 의미 - 약속된 규칙 
+// 함수기능() 
+
+// 함수 이름으로 기능을 유추해보세요.
+// 직관적이고 쉬운 함수 이름이 잘 지어진 이름입니다!!!
+//playMusic()
+//stopMusic()
+//pauseMusic()
+
+// 적절하지 않은 함수 이름
+// function return() {}
+// function 2break() {}
+// function cont inue() {}
+// function cla--ss() {}
+// function for() {}
