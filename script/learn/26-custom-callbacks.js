940c0c281e7284ee8491f818a6f32d9bc98d9708
HanUL072 <dbstjrals125@naver.com>
2025-06-17 09:50:09 +0900

    [학습] 이벤트 헬퍼(유틸) 함수 - helper functions - util functions

--- script/learn/26-custom-callbacks.js
+++ script/learn/26-custom-callbacks.js
@@ -1,5 +1,56 @@
+// const button1 = document.querySelector('.button-1')
+// const button2 = document.querySelector('.button-2')
+
+
+
+// 실습2 : 마우스 오버했을 때와 하지 않았을 때 문구가 나옴 + (클래스가 추가되어)색이 바뀜
+// - mouseenter(element, listener)
+const body = document.body
 const button1 = document.querySelector('.button-1')
 const button2 = document.querySelector('.button-2')
+const button3 = document.querySelector('.button-3')
+const button4 = document.querySelector('.button-4')
+const button5 = document.querySelector('.button-5')
+
+// Mouse EventAdd commentMore actions
+// - click
+// - dblclick
+// - mousedown
+// - mousemove
+// - mouseup
+// - mouseenter(element, listener)
+// - mouseleave(element, listener)
+
+const handleActive = () => {
+  body.classList.add('is-active')
+  button1.classList.add('is-active')
+  button2.classList.add('is-active')
+  button3.classList.add('is-active')
+  button4.classList.add('is-active')
+  button5.classList.add('is-active')
+}
+
+const handleInactive = () => {
+  body.classList.remove('is-active')
+  button1.classList.remove('is-active')
+  button2.classList.remove('is-active')
+  button3.classList.remove('is-active')
+  button4.classList.remove('is-active')
+  button5.classList.remove('is-active')
+}
+
+button1.addEventListener('mouseenter', handleActive)
+button1.addEventListener('mouseleave', handleInactive)
+button2.addEventListener('mouseenter', handleActive)
+button2.addEventListener('mouseleave', handleInactive)
+button3.addEventListener('mouseenter', handleActive)
+button3.addEventListener('mouseleave', handleInactive)
+button4.addEventListener('mouseenter', handleActive)
+button4.addEventListener('mouseleave', handleInactive)
+button5.addEventListener('mouseenter', handleActive)
+button5.addEventListener('mouseleave', handleInactive)
+
+
 
 click(document.querySelector('.button-1'), () => console.log('button 1'))
 click(document.querySelector('.button-2'), () => console.log('button 2'))
@@ -23,3 +74,6 @@ function callToAction(callback) {
 // callToAction(() => {
 //   console.log('콜백은 다른 함수에 인수로 전달되는 함수를 말합니다.')
 // })
+
+
+
---
+++ script/utils/events.js
@@ -0,0 +1,9 @@
+function click(element, listener) {
+  element.addEventListener('click', listener)
+}
+function mouseenter(element, listener) {
+  element.addEventListener('mouseenter', listener)
+}
+function mouseleave(element, listener) {
+  element.addEventListener('mouseleave', listener)
+}
