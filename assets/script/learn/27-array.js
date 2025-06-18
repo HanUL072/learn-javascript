711441ebea94e8d7c6559e4f97f43b8229cf243a
HanUL072 <dbstjrals125@naver.com>
2025-06-18 11:53:38 +0900

    [실습] JavaScript 배열(Array) - 배열 생성 - 배열 값 읽기 - 배열 값 쓰기

--- assets/script/learn/27-array.js
+++ assets/script/learn/27-array.js
@@ -70,3 +70,47 @@ let secondFromTheEndItem2 = 장바구니.at(-2)
 let lastCartItem2 = 장바구니.at(- 1)
 console.log(fistCartItem2, secondFromTheEndItem2, lastCartItem2)
 
+// 배열 원소(항목) 값 읽기
+console.log(장바구니[2])
+console.log(장바구니.at(2))
+
+// 
+장바구니[2] = '순두부'
+console.log(장바구니[2])
+
+
+// 1. 빈 배열을 생성
+const 객체 = []
+console.log(객체)
+
+// 2. 3개의 항목을 포함하는 배열 작성
+const 야구선수 = ['김도영', '이정후', '오타니']
+console.log(야구선수)
+
+// 배열에 대해 이해할 수 있는 연습을 진행합니다.
+// 빈 배열을 생성합니다.
+// 3개의 항목을 포함하는 배열을 작성합니다.
+
+// 3. instructors 배열에서 "야무" 항목을 가져와 yamoo9 변수에 할당합니다.
+// 4. instructors 배열에서 "슬비" 항목 값을 "김데레사"로 변경합니다.
+const instructors = ['야무', '슬비']
+console.log(instructors)
+
+instructors[0] = 'yammo9'
+instructors[1] = '김데레사'
+
+
+// 배열에서 2개 이상의 값을 꺼내고 싶을 때
+// 장바구니2[0]과 장바구니2[1]을 같이 꺼내고 싶다면 어떻게 할까요?
+const 장바구니2 = ['빵', '과자', '햄스터', '고양이']
+
+// 초급 수준에서의 답변
+let 첫번째_상품, 두번째_상품
+
+첫번째_상품 = 장바구니2[0]
+두번째_상품 = 장바구니2.at(1)
+
+console.table({ 첫번째_상품, 두번째_상품 })
+
+// ES2015(6) 수준에서의 답변
+// 구조 분해 할당(Destructuring Assignment)
