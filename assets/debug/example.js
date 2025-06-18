0f6411ca7549c1f3055ca87c934fb97fe4280ab1
HanUL072 <dbstjrals125@naver.com>
2025-06-18 10:37:21 +0900

    [학습] 린터(검사도구) - ESLint 검사도구 설치 - 빠른 시작 구성 - 명령어 검사 - VS Code 확장 설치 및 활용

--- assets/debug/example.js
+++ assets/debug/example.js
@@ -1,6 +1,25 @@
-const button = document.querySelector('.button')
-// console.log('button')
+// const button = document.querySelector('.button')
+// // console.log(button)
 
-button.addEventListener('click', e => {
+// button.addEventListener('click', () => {
+//   console.log('버튼 클릭!')
+// })
+
+// let condition = true
+
+// if (condition) {
+//   console.log('참입니다!')
+// } else {
+//   console.log('거짓입니다!')
+// }
+
+
+// ----------------------
+let link
+
+link = document.querySelector('.link')
+console.log(link)
+
+link.addEventListener('click', () => {
   console.log('버튼 클릭!')
 })
