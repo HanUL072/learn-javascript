3818093072d5ed83a80d692a01b105c9ead1eeec
HanUL072 <dbstjrals125@naver.com>
2025-06-19 11:09:44 +0900

    [학습] do...while 문 조건과 상관없이 반드시 한 번은 실행되는 반복문

--- assets/script/learn/30-loop.js
+++ assets/script/learn/30-loop.js
@@ -35,7 +35,7 @@
 // 특정 상황(조건) 설정
 // let i = 10
 
-// while (wantToDraw) {
+// while(wantToDraw) {
 //   console.log('while문')
 //   draw()
 
@@ -43,6 +43,7 @@
 //   // 조건을 거짓으로 만들께요! (멈춤)
 //   // wantToDraw = !wantToDraw
 //   if (i-- <= 0) { // 현재 라인 기준 - 선감소(--i): i = 9  / 후감소(i--): i = 10
+//   // 조건을 참에서 거짓으로 변경
 //     wantToDraw = false
 //     console.log('i =', i, 'wantToDraw =', wantToDraw)
 //   } else{
@@ -50,7 +51,7 @@
 //     // i -= i // 10 = 1 -> 9, 8, 7, 6, 5, 4, 3, 2, 1
 
 //     // 후감소
-//     i--
+//     // i--
   
 //    // 선감소
 //   //  --i
@@ -92,82 +93,140 @@
 // ------------------------------------------------
 // 흐름 제어 (continue, break)
 
-;(() => {
+// ;(() => {
 
-  let drawCount = 20
-  let i = 0
+//   let drawCount = 20
+//   let i = 0
 
-  const draw = (n) => {
-    console.log('도화지에 그림을 그린 횟수 =' + n)
-  }
+//   const draw = (n) => {
+//     console.log('도화지에 그림을 그린 횟수 =' + n)
+//   }
 
-  // continue
-  // while(i++ < drawCount /* 0 ~ 10 -> continue -> 16~ 19*/) {
-  //   // i += 1
-  //   // ++i
-  //   // i++
+//   // continue
+//   // while(i++ < drawCount /* 0 ~ 10 -> continue -> 16~ 19*/) {
+//   //   // i += 1
+//   //   // ++i
+//   //   // i++
 
-  //   // i = 10
-  //   if (i > 10 && i <= 15) {
-  //     continue
-  //   }
+//   //   // i = 10
+//   //   if (i > 10 && i <= 15) {
+//   //     continue
+//   //   }
 
-  //   // continue 를 만나면 해당 코드 무시
-  //   draw(i)
-  // }
-
-
-  // break
-  while(i++ < drawCount /* 0 < 20 */) {
-    // i = 1, 2, 3, 4, 5
-
-    if (i === 5) {
-      break // 반복문 종료
-    }
-
-    draw(i)
-  }
-
-  console.log('last i=', i)
+//   //   // continue 를 만나면 해당 코드 무시
+//   //   draw(i)
+//   // }
 
 
-})()
+//   // break
+//   while(i++ < drawCount /* 0 < 20 */) {
+//     // i = 1, 2, 3, 4, 5
 
-;(() => {
-  // let userInput = Number.parseInt(prompt('1부터 10까지 숫자를 입력하세요.'), 10);
+//     if (i === 5) {
+//       break // 반복문 종료
+//     }
 
-  // while (Number.isNaN(userInput) || userInput < 1 || userInput > 10) {
-  //   console.error('입력 값이 1부터 10 사이의 숫자여야 합니다.');
-  //   userInput = Number.parseInt(prompt('1부터 10까지 숫자를 입력하세요.'), 10);
-  // }
+//     draw(i)
+//   }
 
-  // console.log('userInput =', userInput)
+//   console.log('last i=', i)
 
 
-  // 선 증가, 후 증가는 왜 쓰는 지, 어떨 때 쓰는지 궁금합니다! 사실 아직 잘 이해가 안돼서요..ㅠ
+// })()
+
+
+
+// ;(() => {
+//   // let userInput = Number.parseInt(prompt('1부터 10까지 숫자를 입력하세요.'), 10);
+
+//   // while (Number.isNaN(userInput) || userInput < 1 || userInput > 10) {
+//   //   console.error('입력 값이 1부터 10 사이의 숫자여야 합니다.');
+//   //   userInput = Number.parseInt(prompt('1부터 10까지 숫자를 입력하세요.'), 10);
+//   // }
+
+//   // console.log('userInput =', userInput)
+
+
+//   // 선 증가, 후 증가는 왜 쓰는 지, 어떨 때 쓰는지 궁금합니다! 사실 아직 잘 이해가 안돼서요..ㅠ
   
-  // let i = 0
+//   // let i = 0
 
-  // step 1.
-  // while(i < 10) {
-  //   console.log(i)
-  //   i = i + 1
-  // }
+//   // step 1.
+//   // while(i < 10) {
+//   //   console.log(i)
+//   //   i = i + 1
+//   // }
 
-  // step 2.
-  // while(i < 10) {
-  //   console.log(i)
-  //   // i = i + 1
-  //   // i += 1
-  //   i++
-  // }
+//   // step 2.
+//   // while(i < 10) {
+//   //   console.log(i)
+//   //   // i = i + 1
+//   //   // i += 1
+//   //   i++
+//   // }
 
-  // step 3.
-  // while(i++ < 10) {
-  //   console.log(i)
-  // }
+//   // step 3.
+//   // while(i++ < 10) {
+//   //   console.log(i)
+//   // }
   
-  // step 4.
-  // while(i++ < 10) console.log(i)
+//   // step 4.
+//   // while(i++ < 10) console.log(i)
+
+// })
+
+
+// ---------------------------------------------------------------
+// 반복문 (do ... while)
+// while vs. do ... while
+
+
+// ;(() => {
+//   let condition = false
+
+//   // do ... whild 문
+//   // 코드가 조건과 상관없이 한번은 반드시 실행되면 좋겠다.
+//   // 조건이 거짓이어도 1번은 반드시 실행
+  
+//   do {
+//     console.log('condition is true')
+//   } while(condition)
+
+
+//   // while 문
+//   while(condition) {
+//     // 조건이 거짓이면 1번도 실행 안함
+//     console.log('condition is true')
+//   }
+// })
+
+// --------------------------------------------------------------------------Add commentMore actions
+// 사용자 입력 검증 (while)
+// "1부터 10까지 숫자를 입력하세요"
+
+// do...while 문 사용 예시
+;(() => {
+  let userInput
+
+  do {
+    userInput = parseInt(prompt("1부터 10까지 숫자를 입력하세요"), 10)
+    if (Number.isNaN(userInput) || userInput < 1 || userInput > 10) 
+      console.error('입력 값이 1부터 10까지 숫자여야 합니다.')
+  } while (Number.isNaN(userInput) || userInput < 1 || userInput > 10)
+
+  console.log('사용자가 입력한 값은 ' + userInput + '입니다.')
+})
+
+// while 문 사용 예시
+;(() => {
+  let userInput = parseInt(prompt("1부터 10까지 숫자를 입력하세요"), 10)
+
+  while (Number.isNaN(userInput) || userInput < 1 || userInput > 10) {
+    console.error('입력 값이 1부터 10까지 숫자여야 합니다.')
+    userInput = parseInt(prompt("1부터 10까지 숫자를 입력하세요"), 10)
+  }
+  
+  console.log('사용자가 입력한 값은 ' + userInput + '입니다.')
 
 })
+// -----------------------------------------------------------------------
