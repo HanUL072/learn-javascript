27196fc6ce295c55708b2ea6d2e5015e0accfb98
HanUL072 <dbstjrals125@naver.com>
2025-06-24 16:31:58 +0900

    [학습] attr 유틸리티 함수 - 기능(함수) 추추출 - 특정 함수 내부에 재사용 가능한 작은 코드가 눈에 보이면 재사용할 수 있게 작은 함수로 추출

--- assets/scripts/utils/css.js
+++ assets/scripts/utils/css.js
@@ -26,13 +26,15 @@ function css(element, propertyName, propertyValue, pseudoElement) {
   // [스타일 쓰기] 속성 값이 전달되었을 때
   if (propertyValue) {
     // 스타일 쓰기
-    const styledElement = setStyle(element, propertyName, propertyValue)
-    return styledElement
+    // const styledElement = setStyle(element, propertyName, propertyValue)
+    // return styledElement
+    return setStyle(element, propertyName, propertyValue)
   } 
   // [스타일 읽기] 속성 값이 전달되지 않았을 때
   else {
     // 스타일 읽기
-    const propertyValue = getStyle(element, propertyName, pseudoElement)
-    return propertyValue
+    // const propertyValue = getStyle(element, propertyName, pseudoElement)
+    // return propertyValue
+    return getStyle(element, propertyName, pseudoElement)
   }
 }
