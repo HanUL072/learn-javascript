6ab8491415d9ed5a404e1331cb92e7be5a41dd64
HanUL072 <dbstjrals125@naver.com>
2025-07-02 15:50:55 +0900

    [학습] 유용한 JS 기능 - 빠른 반환2

--- assets/scripts/learn/48-early-return.js
+++ assets/scripts/learn/48-early-return.js
@@ -37,7 +37,7 @@
 
   function hasOwnClassName(element) {
 
-    
+
     // 빠른 시점에 함수 값을 반환(return)한다.
     // 중첩된 코드를 줄이고, else 구문을 제거한다.
     // = 이미 거짓이 된 경우가 확인 됬을 경우, 뒤의 코드는 실행하지 마셈
@@ -60,4 +60,56 @@
   const result = hasOwnClassName(body)
   console.log(result)
 
+})()
+
+
+;(() => {
+
+  // 빠른 반환을 사용하지 않은 경우
+  function getGift1(score) {
+    let gift = null
+
+    if (score === 100) {
+      gift = '노트북'
+    } else if (score > 75) {
+      gift = '태블릿'
+    } else if (score > 50) {
+      gift = '스마트폰'
+    }
+
+    return gift
+  }
+
+  // 빠른 반환을 사용하는 경우
+  function getGift2(score) {
+    if (score === 100) return '노트북'
+    if (score > 75) return '태블릿'
+    if (score > 50) return '스마트폰'
+    return null
+  }
+    
+
+  const gift1 = getGift1(55)
+  const gift2 = getGift2(100)
+  console.log(gift1) 
+  console.log(gift2) 
+  
+  // 빠른 반환을 사용하지 않은 경우
+  // container.addEventListener('click', (e) => {
+  //   const el = e.target.closest('.element')
+  //   if (el) {
+  //     el.classList.toggle('.toggle')
+  //   }
+  // })
+
+  // 빠른 반환을 사용하는 경우
+  // container.addEventListener('click', (e) => {
+  //   const el = e.target.closest('.element')
+    
+  //   if (!el) return 
+
+  //   el.classList.toggle('.toggle')
+  // })
+
+  
 })()
