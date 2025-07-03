097f0232b3995f8ae711ea2ecf59f7276774e9b9
HanUL072 <dbstjrals125@naver.com>
2025-07-03 14:29:16 +0900

    [학습] 유용한 JS 기능 - 배열 전개 구문 - concat()과 비교 - Array.from()과 비교

---
+++ assets/scripts/learn/53-rest-parameters-and-spread-syntax.js
@@ -0,0 +1,83 @@
+// -------------------------------------------------------
+// 📌 나머지 매개변수 / 전개 구문 (Reset Prameters and Spread Syntax)
+// --------------------------------------------------------
+
+// 전개 연산자(...)
+
+// 배열 전개
+;(() => {
+
+  const breads = ['크림빵', '소보로빵', '소금빵']
+
+  // 문자열 값
+  // console.log(`${breads[0]}, ${breads[1]}, ${breads.at(-1)}`)
+  // console.log(breads.toString())
+
+  // 구조 분해 할당 활용
+  // const [x, y, z] = breads
+  // console.log(x, y, z)
+
+  // 전개 구문 활용
+  console.log(...breads) // cosole.log(breads[0], breads[1], breads[2])
+  // console.log('크림빵', '소보로빵', '소금빵')
+
+})
+
+
+;(() => {
+
+  const messages = ['hello', 'hi', 'how are you?']
+
+  function log(message1, message2, message3) {
+    console.log(message1)
+    console.log(message2)
+    console.log(message3)
+  }
+
+  // log(messages[0], messages[1], messages[2])
+  log(...messages)
+})
+
+// 다른 배열 내부에도 전개 가능
+;(() => {
+
+  const team = ['one', 'two', 'three']
+
+  const anotherTeam = ['four', ...team ,'six', 'nine']
+
+  console.log(anotherTeam)
+})
+
+
+// .concat() 메서드 vs. 전개 구문
+;(() => {
+  const itemToAdd = 1
+  const existingArray = [2, 3]
+
+  // .concat()
+  const arr = []
+  arr.push(itemToAdd)
+  let combinedArr = arr.concat(existingArray)
+  console.log(combinedArr)
+
+  // spread syntax
+  combinedArr = [itemToAdd, ...existingArray]
+  console.log(combinedArr)
+
+})
+
+// HTMLCollection of NodeList -> Array 변환
+// Array.from() vs. Spread Syntax
+;(() => {
+
+  // const mainChildren = document.querySelector('main').children
+
+  const mainChildrenArray = Array.from(document.querySelector('main').children)
+  const bodyChildrenArray = [...document.body.children]
+
+  console.log(mainChildrenArray)
+  console.log(bodyChildrenArray)
+  
+  // console.log(Array.from(mainChildren))
+  // console.log([...mainChildren])
+})
