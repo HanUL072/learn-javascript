0ddd483f7434e9e5e6763f4303fc3421dea3b9c6
HanUL072 <dbstjrals125@naver.com>
2025-07-11 15:21:47 +0900

    [학습] 디바운스(debounce) 조정 호출 - 특정 함수의 실행 빈도를 제한하는 방법 - 과도한 호출로 인한 성능 저하 방지 - 사용자가 연속 입력할 경우, 이전에 타임아웃에 등록된 콜백 함수는 클린업(정리) - 사용자 입력이 중단되 후 일정 시간이 소요되면 타임아웃에 등록된 함수 1회 호출

---
+++ assets/scripts/utils/throttle.js
@@ -0,0 +1,37 @@
+
+/* eslint-disable no-unused-vars */
+
+// 쓰로틀 함수 작성
+// 함수(전달받은함수) -> 반환한함수
+function throttle(callback, delay = 400) {
+  let timeout // undefined
+
+  // 반환 함수
+  return function eventListener(...args) {
+    // 발생 빈도 조정
+
+    if (!timeout) {
+        timeout = setTimeout(() => {
+        // 이벤트 리스너의 매개변수 집합(배열) 전개
+         callback(...args)
+      }, delay)
+    }
+  }
+} 
+
+// 쓰로틀 함수 사용
+const eventListener1 = throttle((e) => {
+  console.log(e)
+}) // 0.4초 마다 주기적으로 함수 호출
+const eventListener2 = throttle((e) => {
+  console.log(e)
+}, 300) // 0.3초 마다 주기적으로 함수 호출
+
+// 이벤트 리스너로 추가
+globalThis.addEventListener('scroll', eventListener1)
+globalThis.addEventListener('resize', eventListener2)
+
+
+// globalThis.addEventListener('resize', function eventListener(e) {
+//   console.log(e.type, e.currentTarget, e.target, e.defaultPrevented)
+// })
